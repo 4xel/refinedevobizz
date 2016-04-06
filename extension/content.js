7e8d4c44e07994d47f8b0aad61ed24e234e6abfa
Ryan De Gruyter <degruyter.ryan@gmail.com>
2016-04-06 09:59:41 +0200

    Add constraint for national holidays and weekend

--- extension/content.js
+++ extension/content.js
@@ -19,11 +19,13 @@ function putDefaultHours(data) {
             .contents()
             .find('frame[name="prbody"]')
             .contents()
-            .find('table tr')
-            .not(':contains("Za")')
-            .not(':contains("Zo")');
+            .find('table tr');
 
-    $($tableRows).find('td input[name*='+inputNameHours+']').val(data.hours);
-    $($tableRows).find('td select[name*='+inputNameType+']').val(data.type);
-    $($tableRows).find('td input[name*='+inputNameComment+']').val(data.comment);
+    $($tableRows).find('td input[name*='+inputNameHours+']').not(isDayOff).val(data.hours);
+    $($tableRows).find('td select[name*='+inputNameType+']').not(isDayOff).val(data.type);
+    $($tableRows).find('td input[name*='+inputNameComment+']').not(isDayOff).val(data.comment);
+
+    function isDayOff(index,el){
+        return $(el).css('background-color') === "rgb(255, 255, 102)";
+    }
 }
