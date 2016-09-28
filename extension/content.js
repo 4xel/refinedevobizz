112b9ad1bc165757e82e704fea64033d323eb75a
Axel Bergmans <github@4xel.be>
2016-09-28 11:11:24 +0200

    Add Extra info field for 'OPL'

--- extension/content.js
+++ extension/content.js
@@ -11,6 +11,7 @@ function putDefaultHours(data) {
     var inputNameHours = "EVOD";
     var inputNameType = "EVOE";
     var inputNameComment = "EVOC";
+    var inputExtraInfo = "EVOK";
 
     var $tableRows =
         $('frame[name="loginscreen"]')
@@ -24,6 +25,13 @@ function putDefaultHours(data) {
     $($tableRows).find('td input[name*='+inputNameHours+']').not(isDayOff).val(data.hours);
     $($tableRows).find('td select[name*='+inputNameType+']').not(isDayOff).val(data.type);
     $($tableRows).find('td input[name*='+inputNameComment+']').not(isDayOff).val(data.comment);
+    if (data.type == 'OPL'){
+    $($tableRows).find('td select[name*='+inputExtraInfo+']').not(isDayOff).val(data.extraInfo);
+    $($tableRows).find('td select[name*='+inputExtraInfo+']').not(isDayOff).prop("disabled", false);
+    } else {
+    $($tableRows).find('td select[name*='+inputExtraInfo+']').not(isDayOff).val("");
+    $($tableRows).find('td select[name*='+inputExtraInfo+']').not(isDayOff).prop("disabled", true);
+    }
 
     function isDayOff(index,el){
         return $(el).css('background-color') === "rgb(255, 255, 102)";
--- extension/popup.js
+++ extension/popup.js
@@ -6,11 +6,21 @@ document.addEventListener('DOMContentLoaded', function () {
         var message = {
             hours: document.getElementById('hours').value,
             type: document.getElementById('type').value,
-            comment: document.getElementById('comment').value
+            comment: document.getElementById('comment').value,
+            extraInfo: document.getElementById('extraInfo').value
         };
 
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, message);
         });
     });
+    document
+        .querySelector('#type')
+        .addEventListener('change', function () {
+          if (document.getElementById('type').value == 'OPL'){
+            document.getElementById('extra').style.display = 'block';
+          } else {
+            document.getElementById('extra').style.display = 'none';
+          }
+        });
 });
